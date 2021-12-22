d024442e3a34cc7c59424ddcbb1395d0bc97b5b6
Molly Wobbles <molly.rendon@outlook.com>
2021-12-21 19:21:43 -0500

    Added employee prompt questions

--- index.js
+++ index.js
@@ -61,8 +61,76 @@ const addManager = () => {
 const addEmployee = () => {
     return inquirer.prompt ([
         {
-            
-        }
+            type: 'list',
+            name: 'role',
+            message: 'Choose your employee role from the following options.',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the employee?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("You need to enter the name of the employee to continue");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the ID of the employee?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter the employee ID to continue!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employee email?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter the employeer email to continue!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the employee github username?',
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log ('You need to enter the employee github username to continue!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the name of the intern's school?",
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log ("You need to enter the intern school to continue!")
+                }
+            }
+        },
     ])
 }
 
