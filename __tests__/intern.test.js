9dabc1d6681c13d4c6c4e79df7edcec48b0f4f74
Molly Wobbles <molly.rendon@outlook.com>
2021-12-22 20:10:53 -0500

    Updated intern test file

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -7,8 +7,16 @@ test('test to create a new intern object', () => {
     expect(intern.school).toEqual(expect.any(String));
 });
 
+/*Test to get school name from getSchool*/
 test('test to get the school of the employee', () => {
     const intern = new Intern('Sheridan', 20, 'sheridanb5@gmail.com', 'B5SU');
 
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+/*Test to get the employee role from getRole*/
+test('test to get employee role', () => {
+    const intern = new Intern('Sheridan', 20, 'sheridanb5@gmail.com', 'B5SU');
+
+    expect(intern.getRole()).toEqual('Intern');
 });
